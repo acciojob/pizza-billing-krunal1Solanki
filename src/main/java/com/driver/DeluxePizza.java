e6324c66333a7f81839b1857dca32a57f74afa9f
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-01-18 15:37:38 +0530

    k

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,8 +4,10 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        //will called parent constructor
+        // your code goes here
+        // below methods are by defaults becouse its deluxe pizza
         addExtraCheese();
         addExtraToppings();
-
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,39 +3,65 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    private final Boolean isVeg;
     private String bill;
-    private int extraCheese;
-    private int extreToppings;
-    private int bag;
+    private boolean ischeese=false;
+    private boolean istoppings=false;
+    private boolean ispaperbag=false;
+
     public Pizza(Boolean isVeg){
-        if(isVeg) price += 300;
-        else price += 400;
-        extraCheese = 80;
-        extreToppings = 70;
         this.isVeg = isVeg;
+        // your code goes here
+        if(isVeg){
+            this.price=300; // veg-pizza
+        }else {
+            this.price=400; // non-veg pizza price
+        }
+        return;
     }
 
     public int getPrice(){
+        this.bill+=this.price;
         return this.price;
     }
 
     public void addExtraCheese(){
-        this.price += extraCheese;
+        /* your code goes here */
+        if(!this.ischeese){
+            this.ischeese=true;
+            this.price+=80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        this.price += extreToppings;
+        if (isVeg && !this.istoppings){
+            this.istoppings = true;
+            this.price += 70;
+//            String toppings = "Extra Toppings Added: 70\n";
+        }
+        else if (!isVeg && !this.istoppings){
+            this.istoppings = true;
+            this.price += 120;
+//            String toppings = "Extra Toppings Added: 120\n";
+        }
+        return;
     }
 
     public void addTakeaway(){
         // your code goes here
-        this.price += bag;
+        if (!this.ispaperbag){
+            this.ispaperbag = true;
+            this.price += 20;
+//            String paperbag = "Paperbag Added: 20\n";
+        }
+
+        return;
     }
 
     public String getBill(){
         // your code goes here
+        this.bill =("Base Price Of The Pizza: "+(isVeg?300:400)+"\nExtra Cheese Added: 80\nExtra Toppings Added: "+(isVeg?70:120)+(ispaperbag?"\nPaperbag Added: 20":"")+"\nTotal Price: "+this.price+"\n");
         return this.bill;
     }
 }
